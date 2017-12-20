f0c6914fb2a7653e8d67e1480261a613379ec882
Bradley Kitchen <bradkitchen2004@sbcglobal.net>
2017-12-20 03:08:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function iLoveTheBeatles(number) {
   var array = [];
   do {
     array.push("I love the Beatles!");
-    while (number < 25);
-    number++;
+    while (number < 15);
   }
+  return array;
 }
